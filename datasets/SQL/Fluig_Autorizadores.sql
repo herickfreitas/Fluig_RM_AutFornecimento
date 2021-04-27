9f92cb1c4ebba483f3d2b34d221898760d894323
Herick Freitas <herickfreitas@gmail.com>
2021-04-27 10:22:25 -0300

    manutenção
    atribuindo por usuário ao invés de por grupo

--- datasets/SQL/Fluig_Autorizadores.sql
+++ datasets/SQL/Fluig_Autorizadores.sql
@@ -1,7 +1,7 @@
-USE [Corporerm_Homolog]
+USE [corporerm]
 GO
 
-/****** Object:  UserDefinedFunction [dbo].[Fluig_Autorizadores]    Script Date: 08/03/2021 10:36:27 ******/
+/****** Object:  UserDefinedFunction [dbo].[Fluig_Autorizadores]    Script Date: 26/04/2021 19:15:02 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -23,28 +23,28 @@ AS
 		
 		DECLARE @Retorno AS VARCHAR(17)
 	
-		-- Atribuido valor na vari√°vel autorizador, para preenchimento do formul√°rio  (SUBSTRING(@CENTROCUSTO,0,11)
+		-- Atribuido valor na vari·vel autorizador, para preenchimento do formul·rio  (SUBSTRING(@CENTROCUSTO,0,11)
     	if  (SUBSTRING(@CENTROCUSTO,0,12) = ('12.01.02.01'))
-    		SELECT @Retorno = 'Pool:Group:w_SG' -- COMO SG - SIMONEGUIMARAES
+    		SELECT @Retorno = 'simoneguimaraes' --'Pool:Group:w_SG' -- COMO SG 
     		
     	else if (@CENTROCUSTO = ('20.01.02.08.70080'))
-    		SELECT @Retorno = 'Pool:Group:w_VPF' -- COMO VPF - LEANDROPINTO		
+    		SELECT @Retorno = 'leandropinto' --'Pool:Group:w_VPF' -- COMO VPF
     	
     	else if (SUBSTRING(@CENTROCUSTO,0,9) = ('20.01.01'))
-			SELECT @Retorno =  'Pool:Group:w_VPF' -- COMO VPF - LEANDROPINTO
+			SELECT @Retorno =  'leandropinto' --'Pool:Group:w_VPF' -- COMO VPF 
     		
     	else if (SUBSTRING(@CENTROCUSTO,0,3) = ('12') OR SUBSTRING(@CENTROCUSTO,0,3) = ('13') 
 			OR SUBSTRING(@CENTROCUSTO,0,3) = ('14') OR 	SUBSTRING(@CENTROCUSTO,0,3) = ('15') 
 			OR SUBSTRING(@CENTROCUSTO,0,3) = ('16'))
-    		SELECT @Retorno =  'Pool:Group:w_GP' -- COMO GP - LenouraSchmidt
+    		SELECT @Retorno =  'lenouraschmidt' -- 'Pool:Group:w_GP' -- COMO GP 
     	
     	else if (SUBSTRING(@CENTROCUSTO,0,3) = ('11') OR SUBSTRING(@CENTROCUSTO,0,3) = ('18') 
 			OR SUBSTRING(@CENTROCUSTO,0,3) = ('50') OR SUBSTRING(@CENTROCUSTO,0,3) = ('90') 
 			OR SUBSTRING(@CENTROCUSTO,0,3) = ('99'))
-    		SELECT @Retorno =  'Pool:Group:w_VPF' --// COMO VPF - LEANDROPINTO
+    		SELECT @Retorno =  'leandropinto' --'Pool:Group:w_VPF' --// COMO VPF 
     	
     	else
-    		SELECT @Retorno =  'Pool:Group:w_SG' -- // COMO SG - SIMONEGUIMARAES
+    		SELECT @Retorno =  'simoneguimaraes' --'Pool:Group:w_SG' -- // COMO SG 
     	
 		RETURN @Retorno 
 
@@ -52,4 +52,5 @@ AS
     END
 
 GO
+
 
